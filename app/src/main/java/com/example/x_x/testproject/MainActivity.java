627298191c6b68db38deecadf4531b61926f71fc
leyvien <leyvien1410@gmail.com>
2016-08-13 14:02:56 +0700

    test commit change

--- app/src/main/java/com/example/x_x/testproject/MainActivity.java
+++ app/src/main/java/com/example/x_x/testproject/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.x_x.testproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvtest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("vien","test commit change");
+        tvtest = (TextView)findViewById(R.id.tvtest);
+        tvtest.setText("Hello Git");
     }
 }
